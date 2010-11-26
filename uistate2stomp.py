af0470ed1fee21b3105621bab30afa13b8676174
Zaheer Abbas Merali <zaheerabbas@merali.org>
2010-11-26 01:33:35 +0000

    json encode flumotion components list and send over stomp

--- uistate2stomp.py
+++ uistate2stomp.py
@@ -19,6 +19,7 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 import sys
+import copy
 import os
 root = os.path.join('/usr/lib', 'flumotion', 'python')
 sys.path.insert(0, root)
@@ -28,6 +29,7 @@ from orbited import json
 
 from flumotion.component import feed
 from twisted.internet import reactor, defer
+from twisted.internet.task import LoopingCall
 from flumotion.twisted import pb
 from flumotion.common import log, errors
 from flumotion.common.planet import moods
@@ -40,6 +42,7 @@ import optparse
 
 class FluToStomp:
     def __init__(self, args):
+        self._components = []
         log.init()
 
         parser = optparse.OptionParser()
@@ -55,18 +58,20 @@ class FluToStomp:
         parser.add_option('', '--no-ssl',
                           action="store_true", dest="no_ssl",
                           help="disable encryption when connecting to the manager")
-
+        parser.add_option('-s', '--stomp-port', action="store", type="string",
+                          dest="stomp")
         options, args = parser.parse_args(args)
 
         if options.debug:
             log.setFluDebug(options.debug)
 
         if options.usage:
-            usage(args)
+            self.usage(args)
 
-        if not options.manager:
-            usage(args)
-
+        if not options.manager or not options.stomp:
+            self.usage(args)
+        
+        print "need to connect to stomp port %s" % (options.stomp,)
         self.options = options
         connection = connections.parsePBConnectionInfo(options.manager,
                                                        not options.no_ssl)
@@ -87,7 +92,8 @@ class FluToStomp:
         d.addErrback(failed)
         d.addErrback(lambda x: reactor.stop())
         d.addCallback(self.manager_connected)
-        d.addCallback(lambda x: reactor.stop())
+
+        reactor.connectTCP("localhost", int(options.stomp), StompClient())
 
     @defer.inlineCallbacks
     def manager_connected(self, model):
@@ -97,12 +103,61 @@ class FluToStomp:
             planet = psd.result
             print planet
             flows = planet.get('flows')
-            for f in flows:
-                components = f.get('components')
-                for c in components:
-                    print "%s: %s" % (c.get('name'),moods.get(c.get('mood')).name)
+            self._components = flows[0].get('components')
+            for c in self._components:
+                print "%s: %s" % (c.get('name'),moods.get(c.get('mood')).name)
+            print json.encode(self.components())
         except Exception, e:
             print log.getExceptionMessage(e)
+
+    def usage(self, args, exitval=0):
+        print "usage: %s [OPTIONS] -m MANAGER " \
+            "-s STOMPPORT" % args[0]
+        print ''
+        print 'See %s -h for help on the available options.' % args[0]
+        sys.exit(exitval)
+
+    def components(self):
+        print "..."
+        l = []
+        for c in self._components:
+            keys = c.keys()
+            d = {}
+            for k in keys:
+                if k == 'parent':
+                    continue
+                elif k == 'messages':
+                    messages = []
+                    for m in c.get('messages'):
+                        messages.append(str(m))
+                    d['messages'] = messages
+                else:
+                    d[k] = c.get(k)
+            l.append(d)
+        print "zzz"
+        return l
+
+class StompClient(StompClientFactory):
+    status = {}
+    def recv_connected(self, msg):
+        print "Connected with stomp"
+        #self.subscribe("/flumotion/changes")
+        #print "Subscribed"
+        self.timer = LoopingCall(self.send_status)
+        self.timer.start(15)
+        
+    def recv_message(self, msg):
+        print "Message received %r" % (msg['body'],)
+        message = {}
+        try:
+            message = json.decode(msg['body'])
+        except Exception, e:
+            print "Broken Total Request %r" % (e,)
+            return
+
+    def send_status(self):
+        global main
+        self.send("/flumotion/components", json.encode(main.components()))
 
 main = FluToStomp(sys.argv)
 reactor.run()
